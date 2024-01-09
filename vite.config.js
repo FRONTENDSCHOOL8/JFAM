33552095a675eb581e3ed32605253f31be5fc6e1
lin0211 <slinj0211@gmail.com>
2024-01-09 13:09:03 +0900

    chore: 중간점검용 배포완료

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,16 @@ import { resolve } from 'node:path';
 export default defineConfig({
   build: {
     outDir: 'docs',
-    input: {
-      home: resolve(__dirname, 'index.html'),
-      main: resolve(__dirname, 'src/pages/main/index.html'),
-      header: resolve(__dirname, 'src/pages/header/index.html'),
-      footer: resolve(__dirname, 'src/pages/footer/index.html'),
-      landing: resolve(__dirname, 'src/pages/landing/index.html'),
-      login: resolve(__dirname, 'src/pages/login/index.html'),
-      signup: resolve(__dirname, 'src/pages/signup/index.html'),
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'src/pages/main/index.html'),
+        header: resolve(__dirname, 'src/pages/header/index.html'),
+        footer: resolve(__dirname, 'src/pages/footer/index.html'),
+        landing: resolve(__dirname, 'src/pages/landing/index.html'),
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+        signup: resolve(__dirname, 'src/pages/signup/index.html'),
+      },
     },
   },
 });
