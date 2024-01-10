3838722521f20020ea9b89394d1e17bab4b5ef78
정예슬 <jeong-yeseul@jeong-yeseul-ui-MacBookAir.local>
2024-01-10 13:33:24 +0900

    feat: landing 페이지 썸네일을 포켓베이스에서 불러오는 함수 추가

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,5 +1,7 @@
 import Swiper from 'swiper';
 import gsap from 'gsap';
+import pb from '/src/js/pocketbase.js';
+import manageData from '/src/js/response.js'
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
@@ -30,3 +32,27 @@ document.addEventListener("scroll", function() {
 // gsap
 gsap.from(".p-copywriting",{ y:100, opacity: 0.2, ease: 'steps(30)'});
 
+// 포켓베이스
+
+async function renderSmallThumbnail(){
+  console.log('test1');
+
+  // 경로 문제 404
+  const response = await manageData.get(
+    `https://jfam.pockethost.io/collections/landing_animationImg/records`
+  );
+  console.log('test2');
+
+  const userData = response.data.items;
+  console.log('test3');
+  userData.forEach((item) => {
+    const template = /* html */`
+      <img src="${item.img}" alt="">
+    `;
+    // insertLast 함수 추가
+    document.querySelector('.landing div-slide__one').insertAdjacentHTML('beforeend',template);
+    document.querySelector('.landing div-slide__two').insertAdjacentHTML('beforeend',template);
+  });
+}
+
+renderSmallThumbnail();
