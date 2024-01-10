805ea4a46d32eadc28eb01e04c7f7aa0048cb297
정예슬 <jeong-yeseul@jeong-yeseul-ui-MacBookAir.local>
2024-01-10 17:48:41 +0900

    feat: 포켓베이스 연동 코드 작성

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -29,30 +29,48 @@ document.addEventListener("scroll", function() {
 });
 
 
-// gsap
+// gsap - 적용하면 얘 애니메이션 끝날때까지 기다렸다가 포켓베이스 실행됨
 gsap.from(".p-copywriting",{ y:100, opacity: 0.2, ease: 'steps(30)'});
 
 // 포켓베이스
 
+function getPbImageURL(item,fileName = 'img'){
+  return `https://jfam.pockethost.io/api/files/${item.collectionId}/${item.id}/${item[fileName]}`
+}
+
 async function renderSmallThumbnail(){
-  console.log('test1');
 
-  // 경로 문제 404
-  const response = await manageData.get(
-    `https://jfam.pockethost.io/collections/landing_animationImg/records`
-  );
-  console.log('test2');
+  const response = await pb.collection('landing_animationImg').getList();
+  const userData = response.items;
 
-  const userData = response.data.items;
-  console.log('test3');
   userData.forEach((item) => {
     const template = /* html */`
-      <img src="${item.img}" alt="">
+      <img src="${getPbImageURL(item)}" alt="">
     `;
-    // insertLast 함수 추가
-    document.querySelector('.landing div-slide__one').insertAdjacentHTML('beforeend',template);
-    document.querySelector('.landing div-slide__two').insertAdjacentHTML('beforeend',template);
+    document.querySelector('.landing .div-slide__one').insertAdjacentHTML('beforeend',template);
+    document.querySelector('.landing .div-slide__two').insertAdjacentHTML('beforeend',template);
+    document.querySelector('.landing .div-slide__three').insertAdjacentHTML('beforeend',template);
+    document.querySelector('.landing .div-slide__four').insertAdjacentHTML('beforeend',template);
+
   });
 }
 
+async function renderSwiperThumbnail(){
+
+  const response_swiper = await pb.collection('program_thumbnail').getList();
+  const userData_swiper = response_swiper.items;
+
+  userData_swiper.forEach((item) => {
+    console.log(getPbImageURL(item,"image"));
+    const template = /* html */`
+    <div class="swiper-slide"><img src="${getPbImageURL(item,'image')}" alt="">
+    </div>
+    `;
+    console.log(template);
+    document.querySelector('.landing .swiper-wrapper').insertAdjacentHTML('beforeend',template);
+  });
+}
+
+
 renderSmallThumbnail();
+renderSwiperThumbnail();
