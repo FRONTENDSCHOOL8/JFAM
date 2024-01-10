c46d984f6c6e333d78e9ec2dc7524c43159b85b0
정예슬 <jeong-yeseul@jeong-yeseul-ui-MacBookAir.local>
2024-01-10 11:02:06 +0900

    feat: landing header부분 js 추가

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,4 +1,5 @@
 import Swiper from 'swiper';
+import gsap from 'gsap';
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
@@ -10,3 +11,22 @@ var swiper = new Swiper(".mySwiper", {
     },
  
   });
+
+
+  // 헤더 스크롤 배경
+document.addEventListener("scroll", function() {
+  var header = document.querySelector('.landing .header')
+  var scrollPosition = window.scrollY;
+
+  // 스크롤 위치에 따라 배경을 변화시키기
+  if (scrollPosition > 50) {
+    header.style.backgroundColor = "rgba(0, 0, 0)"; // 스크롤 내리면 배경 불투명
+  } else {
+    header.style.backgroundColor = "rgba(0, 0, 0, 0)"; // 스크롤 올리면 배경 투명
+  }
+});
+
+
+// gsap
+gsap.from(".p-copywriting",{ y:100, opacity: 0.2, ease: 'steps(30)'});
+
