5782e1d91ac737d10b1efba4f2aaaf6db8ccec89
1101x <junggg14@gmail.com>
2024-01-14 15:06:55 +0900

    feature:인트로 효과 수정

--- home.js
+++ home.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
   setTimeout(() => {
     window.location.href = '/src/pages/landing/index.html';
-  }, 3400);
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const audioPlayer = document.querySelector('#audioplayer');
-    audioPlayer.play();
-  });
+  }, 3500);
 });
