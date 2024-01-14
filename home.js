548d233e6fa664b6322748e7afdab9814cef3c1e
1101x <junggg14@gmail.com>
2024-01-14 14:39:07 +0900

    feature: 0114 배포 테스트

--- home.js
+++ home.js
@@ -2,9 +2,4 @@ document.addEventListener('DOMContentLoaded', async () => {
   setTimeout(() => {
     window.location.href = '/src/pages/landing/index.html';
   }, 3400);
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const audioPlayer = document.querySelector('#audioplayer');
-    audioPlayer.play();
-  });
 });
