6a27fd5b49304e96798b71d9aea6f1ec7f6e1223
정예슬 <jeong-yeseul@jeong-yeseul-ui-MacBookAir.local>
2024-01-14 23:53:18 +0900

    feat: header 프로필 display none

--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -1,9 +1,14 @@
 import {getNode} from '/src/js/common.js';
+import { getStorage } from '/src/js/storage';
 
 const buttonProfile = getNode('.button-profile');
 
 function signoutPopup(){
     window.location.href = '/src/pages/profile/'
 }
+
+    if(!getStorage('auth')){
+        getNode('.button-profile').style.display = 'block';
+    }
 
 buttonProfile.addEventListener('click',signoutPopup);
