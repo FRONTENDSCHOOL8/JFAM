74ace91047008e436b1d84fa0236337c3ccb0368
lin0211 <slinj0211@gmail.com>
2024-01-14 21:23:22 +0900

    chore: 배포

--- docs/assets/home-MoAOTyCl.js
+++
@@ -1,1 +0,0 @@
-import"./reset-9GtOEJoH.js";/* empty css              */document.addEventListener("DOMContentLoaded",async()=>{setTimeout(()=>{window.location.href="/src/pages/landing/index.html"},3500)});
