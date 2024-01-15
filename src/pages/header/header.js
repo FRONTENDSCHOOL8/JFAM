be8ed59b4f0596ec6373560d3656d3ab882c03e8
1101x <junggg14@gmail.com>
2024-01-15 13:19:42 +0900

    bug: 메인페이지 깃 충돌 오류

---
+++ src/pages/header/header.js
@@ -0,0 +1,9 @@
+import {getNode} from '/src/js/common.js';
+
+const buttonProfile = getNode('.button-profile');
+
+function signoutPopup() {
+  window.location.href = '/src/pages/profile/';
+}
+
+buttonProfile.addEventListener('click',signoutPopup);
