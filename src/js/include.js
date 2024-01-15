cde4e8b808c7f61261d1d5d83607320c71ae8547
정예슬 <jeong-yeseul@jeong-yeseul-ui-MacBookAir.local>
2024-01-15 11:10:46 +0900

    fix: include.js 수정

--- src/js/include.js
+++ src/js/include.js
@@ -1,66 +1,22 @@
-// function includeHTML(element) {
-//     const targetElement = element;
-//     const { includePath } = targetElement.dataset;
-//     if (includePath) {
-//       const xhttp = new XMLHttpRequest();
-//       xhttp.onreadystatechange = function handleStateChange() {
-//         if (this.readyState === 4 && this.status === 200) {
-//           targetElement.outerHTML = this.responseText;
-//         }
-//       };
-//       xhttp.open('GET', includePath, true);
-//       xhttp.send();
-//     }
-//   }
-  
-//   function loadIncludedHTML() {
-//     const allElements = document.getElementsByTagName('*');
-//     Array.prototype.forEach.call(allElements, includeHTML);
-//     console.log('include');
-//   }
-  
-//   window.addEventListener('load', loadIncludedHTML);
-
-import { getStorage, setStorage } from '/src/js/storage';
-
-function includeHTML(element,onSuccess) {
-  const { includePath } = element.dataset;
+function includeHTML(element) {
+  const targetElement = element;
+  const { includePath } = targetElement.dataset;
   if (includePath) {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function handleStateChange() {
       if (this.readyState === 4 && this.status === 200) {
-        element.outerHTML = this.responseText;
-        onSuccess()
+        targetElement.outerHTML = this.responseText;
       }
     };
-    xhr.open('GET', includePath, true);
-    xhr.send();
+    xhttp.open('GET', includePath, true);
+    xhttp.send();
   }
 }
-// setStorage('auth', {isAuth: false});
-
-// const header = async() =>{
-    
-//   const data = await getStorage('auth');
-//   console.log(data.isAuth);
-//   const buttonProfile = document.querySelector('.button-profile');
-//   if(!data.isAuth){
-//     buttonProfile.style.display = 'none';
-//   }  else{
-//     buttonProfile.style.display = 'block';
-//   }
-// }
 
 function loadIncludedHTML() {
   const allElements = document.getElementsByTagName('*');
-  Array.prototype.forEach.call(allElements, (item)=>{
-    includeHTML(item,()=>{
-      if(item.tagName === 'HEADER') header()
-    })
-  });
+  Array.prototype.forEach.call(allElements, includeHTML);
+  console.log('include');
 }
-
-
-
 
 window.addEventListener('load', loadIncludedHTML);
