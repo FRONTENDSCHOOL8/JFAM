6274fedfc7c9bf56bf8d77ac1c47af0d75c5a040
정예슬 <jeong-yeseul@jeong-yeseul-ui-MacBookAir.local>
2024-01-15 10:16:02 +0900

    fix: header footer 수정중(임시)

--- src/js/include.js
+++ src/js/include.js
@@ -1,22 +1,59 @@
-function includeHTML(element) {
-    const targetElement = element;
-    const { includePath } = targetElement.dataset;
-    if (includePath) {
-      const xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function handleStateChange() {
-        if (this.readyState === 4 && this.status === 200) {
-          targetElement.outerHTML = this.responseText;
-        }
-      };
-      xhttp.open('GET', includePath, true);
-      xhttp.send();
-    }
-  }
+// function includeHTML(element) {
+//     const targetElement = element;
+//     const { includePath } = targetElement.dataset;
+//     if (includePath) {
+//       const xhttp = new XMLHttpRequest();
+//       xhttp.onreadystatechange = function handleStateChange() {
+//         if (this.readyState === 4 && this.status === 200) {
+//           targetElement.outerHTML = this.responseText;
+//         }
+//       };
+//       xhttp.open('GET', includePath, true);
+//       xhttp.send();
+//     }
+//   }
   
-  function loadIncludedHTML() {
-    const allElements = document.getElementsByTagName('*');
-    Array.prototype.forEach.call(allElements, includeHTML);
-    console.log('include');
-  }
+//   function loadIncludedHTML() {
+//     const allElements = document.getElementsByTagName('*');
+//     Array.prototype.forEach.call(allElements, includeHTML);
+//     console.log('include');
+//   }
   
-  window.addEventListener('load', loadIncludedHTML);
+//   window.addEventListener('load', loadIncludedHTML);
+
+function includeHTML(element,onSuccess) {
+  const { includePath } = element.dataset;
+  if (includePath) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        element.outerHTML = this.responseText;
+        onSuccess()
+      }
+    };
+    xhr.open('GET', includePath, true);
+    xhr.send();
+  }
+}
+
+function loadIncludedHTML() {
+  const allElements = document.getElementsByTagName('*');
+  Array.prototype.forEach.call(allElements, (item)=>{
+    includeHTML(item,()=>{
+      if(item.tagName === 'HEADER') header()
+    })
+  });
+}
+
+const header = () =>{
+    
+  let state = false;
+  const ul = document.querySelector('ul');
+  ul.insertAdjacentHTML('beforeend',`${state ? '<li>login</li>' : '<li>logout</li>'}`)
+  
+}
+
+
+
+
+window.addEventListener('load', loadIncludedHTML);
--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -1,14 +1,15 @@
 import {getNode} from '/src/js/common.js';
-import { getStorage } from '/src/js/storage';
+// import { getStorage } from '/src/js/storage';
 
 const buttonProfile = getNode('.button-profile');
+
+// auth 확인 코드 추가, eslint ! 중복사용 오류 있음
+if(!!localStorage.getItem('auth')){
+  buttonProfile.style.display = 'block';
+}
 
 function signoutPopup() {
   window.location.href = '/src/pages/profile/';
 }
-
-    if(!getStorage('auth')){
-        getNode('.button-profile').style.display = 'block';
-    }
 
 buttonProfile.addEventListener('click',signoutPopup);
